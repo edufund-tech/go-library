57fcc70449bf536f79c3b280eb6a2487c9616a3e
Rizki Anurka <rizki.anurka@gmail.com>
2021-01-22 14:12:34 +0700

    set firebase custom claims

--- firebase/firebase.go
+++ firebase/firebase.go
@@ -11,14 +11,15 @@ import (
 
 // User domain model
 type User struct {
-	UID           string `bson:"uid,omitempty" json:"uid,omitempty"`
-	Email         string `bson:"email,omitempty" json:"email,omitempty"`
-	EmailVerified bool   `bson:"email_verified,omitempty" json:"email_verified,omitempty"`
-	PhoneNumber   string `bson:"phone_number,omitempty" json:"phone_number,omitempty"`
-	DisplayName   string `bson:"display_name,omitempty" json:"display_name,omitempty"`
-	PhotoURL      string `bson:"photo_url,omitempty" json:"photo_url,omitempty"`
-	Disabled      bool   `bson:"disabled,omitempty" json:"disabled,omitempty"`
-	Password      string `json:"password,omitempty"`
+	UID           string                 `bson:"uid,omitempty" json:"uid,omitempty"`
+	Email         string                 `bson:"email,omitempty" json:"email,omitempty"`
+	EmailVerified bool                   `bson:"email_verified,omitempty" json:"email_verified,omitempty"`
+	PhoneNumber   string                 `bson:"phone_number,omitempty" json:"phone_number,omitempty"`
+	DisplayName   string                 `bson:"display_name,omitempty" json:"display_name,omitempty"`
+	PhotoURL      string                 `bson:"photo_url,omitempty" json:"photo_url,omitempty"`
+	Disabled      bool                   `bson:"disabled,omitempty" json:"disabled,omitempty"`
+	Password      string                 `json:"password,omitempty"`
+	CustomClaims  map[string]interface{} `json:"custom_claims,omitempty"`
 }
 
 // Client ...
@@ -52,8 +53,11 @@ func (client *Client) Create(ctx context.Context, user User) (*auth.UserRecord, 
 		Password(user.Password).
 		PhotoURL(user.PhotoURL).
 		Disabled(false)
-
 	u, err := client.CreateUser(ctx, params)
+	if err != nil {
+		return nil, err
+	}
+	err = client.SetCustomUserClaims(ctx, u.UID, user.CustomClaims)
 
 	return u, err
 }
