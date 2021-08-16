82404e2e697cefe001d3f019c296ccfd424dcfa0
NKA <ghofinka@gmail.com>
2021-08-16 09:57:24 +0700

    decrypt account info

--- auth/middleman/middleman.go
+++ auth/middleman/middleman.go
@@ -16,13 +16,20 @@ import (
 )
 
 // User domain model
+type EncryptedVerifyResponse struct {
+	Account string `json:"Account"`
+}
+
 type VerifyResponse struct {
 	Account Account `json:"Account"`
 }
 
 type Account struct {
-	ID    string `json:"AccountID"`
-	Code  string `json:"BorrowerID"`
+	ID       string `json:"AccountID"`
+	Code     string `json:"BorrowerID"`
+	Borrower struct {
+		BorrowerID string `json:"BorrowerID"`
+	} `json:"msborrower"`
 	Email string `json:"email,omitempty"`
 }
 
@@ -83,13 +90,45 @@ func Verify(ctx context.Context, token string, permissions map[string]interface{
 		err = errors.New(fmt.Sprintf("Legacy Host Error Code %d (%s)", resp.StatusCode, resp.Status))
 		return verifyResponse, err
 	}
+
+	var encrypted EncryptedVerifyResponse
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		log.Printf("[Middleman Verify] error 4 %v", err)
 		return verifyResponse, err
 	}
+	json.Unmarshal(body, &encrypted)
+
+	r := strings.NewReader(fmt.Sprintf(`{"message": "%s"}`, encrypted.Account))
+	client = &http.Client{}
+
+	decryptURL := ctx.Value("URL_MIDDLEMAN_DECRYPT").(string)
+	if decryptURL == "" {
+		decryptURL = "https://api.edufund.co.id/api/general/decrypt-account"
+	}
+
+	req, err = http.NewRequest("POST", decryptURL, r)
+	if err != nil {
+		log.Printf("[Middleman Verify] error 5 %v", err)
+		return verifyResponse, err
+	}
+	req.Header.Add("Authorization", token)
+	req.Header.Add("Content-Type", "application/json")
+	resp, err = client.Do(req)
+	if err != nil {
+		log.Printf("[Middleman Verify] error 6 %v", err)
+		return verifyResponse, err
+	}
+	defer resp.Body.Close()
+
+	body, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Printf("[Middleman Verify] error 7 %v", err)
+		return verifyResponse, err
+	}
 
 	json.Unmarshal(body, &verifyResponse)
+	verifyResponse.Account.Code = verifyResponse.Account.Borrower.BorrowerID
 
 	return verifyResponse, err
 }
